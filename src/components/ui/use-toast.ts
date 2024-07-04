175197d8ecf7642e15309265b0d8ef2189e2b610
“MahendraDew” <mahendradewangan195@gmail.com>
2024-07-04 17:12:37 +0530

    updated the use-toast import statement for toast component

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -2,11 +2,12 @@
 
 // Inspired by react-hot-toast library
 import * as React from "react"
+import { ToastActionElement, ToastProps } from "./toast"
 
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toast"
+// import type {
+//   ToastActionElement,
+//   ToastProps,
+// } from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
