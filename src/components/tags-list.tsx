bcff3b4b045301011f1b1122cc8e51637f91d722
“MahendraDew” <mahendradewangan195@gmail.com>
2024-07-06 18:54:10 +0530

    updated callback to the search tag in tags-list.tsx to redirect to the /browse/?search page

--- src/components/tags-list.tsx
+++ src/components/tags-list.tsx
@@ -12,7 +12,7 @@ export function TagsList({tags}: {tags: string[]}){
          className={cn(badgeVariants())} 
             key={tag}
             onClick={()=>{
-            router.push(`/?search=${tag}`);
+            router.push(`/browse/?search=${tag}`);
         }} >
             {tag}
         </button>
